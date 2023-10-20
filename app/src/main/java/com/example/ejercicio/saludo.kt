da028507f8cfc25b300b12a35882a282995a78c0
JoelGonzalez2711 <joelgonzalez.alu@iespacomolla.es>
2023-10-20 10:26:40 +0200

    ya funciona dos actividades

--- app/src/main/java/com/example/ejercicio/saludo.kt
+++ app/src/main/java/com/example/ejercicio/saludo.kt
@@ -1,7 +1,7 @@
 package com.example.ejercicio
 
-import android.content.Intent
 import android.os.Bundle
+import android.content.Intent
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -17,24 +17,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ejercicio.ui.theme.EjercicioTheme
 
-const val EXTRA_MESSAGE = "com.example.ejercicio.MESSAGE"
-
-class MainActivity : ComponentActivity() {
+class saludo : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_principal)
-        val EtNombre = findViewById<EditText>(R.id.ET_nombre)
-        val boton = findViewById<Button>(R.id.B_saludame)
+        setContentView(R.layout.layout_saludo)
 
-        boton.setOnClickListener{
-            var nombre = EtNombre.text.toString()
-            val intent=Intent(this, saludo::class.java)
-            intent.putExtra("EXTRA_NOMBRE", nombre)
-            startActivity(intent)
+        val SaludoUsuario=findViewById<TextView>(R.id.saludar_usuario)
+        val respuesta=intent.extras?.getString("EXTRA_NOMBRE").orEmpty()
+        SaludoUsuario.text= "Hola $respuesta"
         }
     }
 
-
-
-
-}
